2a594da29a59cba795ca16d26c4791df84e7955b
kaisernik1 <nikitakaiser@yandex.ru>
2024-12-25 17:03:35 +0300

    ver17

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func checkResourceUsage(stats []string) {
 		fmt.Printf("Free disk space is too low: %d Mb left\n", diskFree)
 	}
 
-	netFree := (netTotal - netUsed) / (1024 * 1024)
+	netFree := ((netTotal / (1024 * 1024)) - (netUsed / (1024 * 1024)))
 	netUsage := float64(netUsed) / float64(netTotal)
 	if netUsage >= networkBandwidthThreshold { 
 		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", netFree)
